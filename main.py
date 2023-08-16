3c7a0e90a9fc972ad66d247b03fe30db530e6f90
Maxztly <maxhantel57@gmail.com>
2023-08-16 11:04:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+from discord import Intents
+import requests
+import math
+
+intents = Intents.default()
+intents.typing = False
+intents.presences = False
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('Bot is ready')
+    await client.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.watching, name=await get_btc_price()))
+
+
+async def get_btc_price():
+    response = requests.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json')
+    data = response.json()
+    price = data['bpi']['USD']['rate']
+
+    # Remove the comma from the price string and then convert it to a float
+    price_without_comma = price.replace(',', '')
+    rounded_price = math.ceil(float(price_without_comma))
+
+    return f'BTC: ${rounded_price} USD'
+
+client.run('YOUR TOKEN')
